7b35c0b5ad922902186eb7229c29582252fc4ab7
Katya Pushkareva <katieperca@yandex.ru>
2022-10-24 00:57:26 +0400

    CORS

--- app.js
+++ app.js
@@ -15,6 +15,29 @@ mongoose.connect('mongodb://localhost:27017/mestodb');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const allowedCors = [
+  'http://mesto.katieperca.nomoredomains.icu',
+  'https://mesto.katieperca.nomoredomains.icu',
+  'http://localhost:3000',
+  'https://localhost:3000',
+];
+
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+  return next();
+});
+
 app.use(requestLogger);
 app.use(routes);
 app.use(errorLogger);
