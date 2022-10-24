bcaac7c7c3d0d985ad655b6b91d52a7b8bef0ac8
Katya Pushkareva <katieperca@yandex.ru>
2022-10-24 15:18:04 +0400

    fix CORS problems

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ app.use((req, res, next) => {
   const requestHeaders = req.headers['access-control-request-headers'];
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', 'true');
   }
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
